39427281b71aa8d780b83cec25daa283fc7f506d
Mark Ariniello <mark.ariniello@colorado.edu>
2014-05-04 15:55:45 -0600

    first commit of the day

--- index.py
+++ index.py
@@ -1,18 +1,27 @@
 import os
+import cgi
 from flask import Flask
 from flask import request
 from flask import render_template
 from flask import json
 from collections import namedtuple as NT
 from flask.ext.sqlalchemy import SQLAlchemy
+import cgitb
+cgitb.enable()
 
+v = int
+v1 = int
+v2 = int
+v3 = int
+v4 = int
+v5 = int
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///photos.db'
 db = SQLAlchemy(app)
 
 class Photo(db.Model):
-	tablename = "photos"
+	tablename = "Photos"
 	id = db.Column(db.Integer, primary_key=True)
 	name = db.Column(db.String(80), unique=True)
 	img_path = db.Column(db.String(120), unique=True)
@@ -21,14 +30,31 @@ class Photo(db.Model):
 	def __init__(self, name, img_path):
 		self.name = name
 		self.img_path = img_path
-
 @app.route('/')
 @app.route('/question')
 def question():
     return render_template('question.html')
+
 @app.route('/simple_view.html')
-def view():
+@app.route('/simple_view.html/<data>')
+def view(data=None):
+#	temp = Photos.query.all()
+#	print temp
+#	dat = None
+#	for item in temp:
+#		if item.id == data:
+#			dat = item.imge_path
 	return render_template('simple_view.html')
+
+form = cgi.FieldStorage()
+v1 = form.getvalue("hi");
+v2 = form.getvalue("waveh");
+v3 = form.getvalue("waves");
+v4 = form.getvalue("what");
+v5 = form.getvalue("ignore");
+
+v = v1 + v2 + v3 + v4 + v5;
+print v
 
 if __name__ == '__main__':
     app.run(debug=True)
--- table_def.py
+++ table_def.py
@@ -3,7 +3,7 @@ from sqlalchemy import Column, String, Integer
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship, backref
 
-engine = create_engine('sqlite:///photos.db', echo=True)
+engine = create_engine('sqlite:///Photos.db', echo=True)
 Base = declarative_base()
 
 #######################################################################
@@ -13,11 +13,14 @@ class Photo(Base):
     id = Column(Integer, primary_key=True)
     name = Column(String)
     img_path = Column(String)
+    mscore = Column(Integer)
     
-    def __init__(self, name, img_path):
+    def __init__(self, name, img_path, mscore):
 		""""""
 		self.name = name
 		self.img_path = img_path
+		self.mscore = mscore
+
 #######################################################################
 
 Base.metadata.create_all(engine)
