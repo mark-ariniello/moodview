7f33f67c2f057a8b6d859fd7bbada6b6e442f3f8
Mark Ariniello <mark.ariniello@colorado.edu>
2014-05-04 19:18:10 -0600

    Everyting work just needs a finished database

--- index.py
+++ index.py
@@ -1,49 +1,53 @@
 import os
 from flask import Flask
 from flask import request
+from flask import redirect
 from flask import render_template
 from flask import json
 from collections import namedtuple as NT
 from flask.ext.sqlalchemy import SQLAlchemy
 
-v = int
-v1 = int
-v2 = int
-v3 = int
-v4 = int
-v5 = int
-
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///photos.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///Photos.db'
 db = SQLAlchemy(app)
 
 class Photo(db.Model):
-	tablename = "Photos"
+	__tablename__ = "Photos"
 	id = db.Column(db.Integer, primary_key=True)
 	name = db.Column(db.String(80), unique=True)
 	img_path = db.Column(db.String(120), unique=True)
+	mscore = db.Column(db.Integer)
 
 
-	def __init__(self, name, img_path):
+	def __init__(self, name, img_path, mscore):
 		self.name = name
 		self.img_path = img_path
+		self.mscore = mscore
+		
 @app.route('/', methods=['POST', 'GET'])
 @app.route('/question', methods=['POST', 'GET'])
 def question():
-	if request.method == 'Post':
-		print request.POST['username']
+	if request.method == 'POST':
+		counter = 0
+		for fnum, item in request.form.items():
+			counter = counter + int(item) 
+			print counter
+		return redirect('/simple_view.html/'+str(counter))
 	return render_template('question.html')
 
 @app.route('/simple_view.html')
 @app.route('/simple_view.html/<data>')
 def view(data=None):
-#	temp = Photos.query.all()
-#	print temp
-#	dat = None
-#	for item in temp:
-#		if item.id == data:
-#			dat = item.imge_path
-	return render_template('simple_view.html')
+	temp = Photo.query.all()
+	dat = None
+	for item in temp:
+		if int(item.mscore) == int(data):
+			dat = item.img_path
+		print item.mscore
+		print item.img_path
+	tempp = str(dat)
+	print tempp
+	return render_template('simple_view.html', image_path=dat)
 
 if __name__ == '__main__':
     app.run(debug=True)
